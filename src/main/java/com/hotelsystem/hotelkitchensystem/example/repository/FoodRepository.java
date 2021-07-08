c383c3b0dce0c959e153880a4c4c79c358aca943
darshana782 <56575929+darshana782@users.noreply.github.com>
2021-07-08 16:08:24 +0530

    ingredients

--- src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/FoodRepository.java
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/FoodRepository.java
@@ -3,7 +3,6 @@ package com.hotelsystem.hotelkitchensystem.example.repository;
 import com.hotelsystem.hotelkitchensystem.example.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface FoodRepository extends JpaRepository<Food, Integer> {
+//    Food findByName(String foodName);
 }
